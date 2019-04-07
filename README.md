6f178003c609ac55d96127b712c80b32539be2cf
DrSnuggles <snugglesdr@hotmail.com>
2019-04-07 09:58:41 +0200

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#JS GonioMeter / VectorScope / CorrelationMeter / PhaseMeter
+
+How it's calced:
+- https://www.kvraudio.com/forum/viewtopic.php?t=477945
+- https://dsp.stackexchange.com/questions/1671/calculate-phase-angle-between-two-signals-i-e-digital-phase-meter
+
+Wanted to make a visualisation for Juergen Wothkes scriptprocessor player
+audiotag.html shows how to connect to an audio tag
---
+++ goniometer.js
@@ -0,0 +1,189 @@
+/*  Goniometer/Phasemeter/VectorScope by DrSnuggles
+    License :
+ */
+
+"use strict";
+
+var Goniometer = (function () {
+  //
+  // Init
+  //
+  var my = {    // public available settings
+  },
+  debug = false, // display console logs?
+  anaL,         // Analyzer for left channel
+  anaR,         // Analyzer for right channel
+  ctx,          // 2D context for drawing, just get it once
+  width,        // width of canvas = calced pixels
+  height,       // height of canvas = calced pixels
+  raf;          // ref to RAF, needed to cancel RAF
+
+  //
+  // Private
+  //
+  function log(out) {
+    if (debug) console.log(out);
+  };
+  function splitChannels(source) {
+    log("splitChannels");
+    var splitter = source.context.createChannelSplitter(2);
+    var left = source.context.createStereoPanner();
+    var right = source.context.createStereoPanner();
+    source.connect(splitter);
+    splitter.connect(left, 0);
+    splitter.connect(right, 1);
+    left.pan.value = -1;
+    right.pan.value = 1;
+    anaL = source.context.createAnalyser();
+    anaR = source.context.createAnalyser();
+    left.connect(anaL);
+    right.connect(anaR);
+  };
+  function renderLoop() {
+    drawGoniometer();
+    //drawScope();
+    /*
+    drawVU(0, 100/15*my.ym.player.voiceA.getvolume());
+    drawVU(1, 100/15*my.ym.player.voiceB.getvolume());
+    drawVU(2, 100/15*my.ym.player.voiceC.getvolume());
+    drawScope(0, my.ym.anal);
+    drawSpectrum(0, my.ym.anal);
+    */
+    raf = requestAnimationFrame(renderLoop);
+  };
+  function drawGoniometerBackground() {
+    // clear old
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'rgb(96, 0, 0)';
+    ctx.beginPath();
+
+    // x - axis
+    ctx.moveTo(0, height/2);
+    ctx.lineTo(width, height/2);
+
+    // y - axis
+    ctx.moveTo(width/2, 0);
+    ctx.lineTo(width/2, height);
+
+    ctx.stroke();
+  };
+  function drawGoniometer() {
+    //log("drawGoniometer");
+    drawGoniometerBackground();
+
+    var dataL = new Uint8Array(anaL.frequencyBinCount);
+    var dataR = new Uint8Array(anaR.frequencyBinCount);
+    anaL.getByteTimeDomainData(dataL);
+    anaR.getByteTimeDomainData(dataR);
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'rgb(0, 96, 0)';
+    ctx.beginPath();
+
+    /* first try
+    // try to apply https://dsp.stackexchange.com/questions/1671/calculate-phase-angle-between-two-signals-i-e-digital-phase-meter
+    // data range = 0 ... 255, 128 should be 0. min/max should be -1/+1
+    ctx.moveTo(width/2, height/2); // mid
+    for (var i = 0; i < dataL.length; i++) {
+      var L = (dataL[i] - 128) / 128;
+      var R = (dataR[i] - 128) / 128;
+      var Phase = Math.atan2(L, R); // nicer then atan coz atan(0/0) is not defined
+      var Magnitude = Math.sqrt(L*L+R*R); // (L**2 + R**2) ** (1/2); // Math.pow(Math.pow(L,2)+Math.pow(R,2), 0.5);
+      var Correlation = Phase * Magnitude;
+      if (Math.abs(Magnitude) > 1 || Math.abs(Correlation) > 1) {
+        // crazy values detected
+        log("L:"+ L +" R: "+ R +" Phase: "+ Phase +" Magnitude: "+ Magnitude +" Correlation: "+ Correlation);
+      }
+      // convert polar into
+
+      ctx.lineTo(Magnitude, Correlation);
+    }
+    */
+
+    // second try : https://www.kvraudio.com/forum/viewtopic.php?t=477945
+    for (var i = 0; i < dataL[i]; i++) {
+      var x = (dataR[i] - 128) / 128; // Right channel is mapped to x axis
+      var y = (dataL[i] - 128) / 128; // Left channel is mapped to y axis
+
+      // Convert cartesian to polar coordinate
+      // @see https://www.mathsisfun.com/polar-cartesian-coordinates.html
+      var radius = Math.sqrt((x * x) + (y * y));
+      var angle = Math.atan(y/x);
+
+      // atan() returns wrong value if either value is negative.
+      // Correct for this by rotating 180 or 360 degrees depending on which
+      // quadrant of the x/y graph the cartesian coordinate is in.
+      if ((x < 0 && y > 0) || (x < 0 && y < 0)) {
+        angle += 3.14159265; // Pi radians = 180 degrees
+      } else if (x > 0 && y < 0) {
+        angle += 6.28318530; // 2Pi radians = 360 degrees
+      }
+
+      // atan() will return zero if either of our coordinates is zero.
+      // Correct for this by manually setting the angle.
+      if (x == 0) {
+        angle = y > 0 ? 1.57079633 : 4.71238898; // 90 or 270 degrees
+      } else if (y == 0) {
+        angle = x > 0 ? 0 : 3.14159265; // 0 or 180 degrees
+      }
+
+      // Rotate coordinate by 45 degrees counter clockwise
+      angle += 0.78539816;
+
+      // Convert polar coordinate back to cartesian coordinate.
+      var xRotated = radius * Math.cos(angle);
+      var yRotated = radius * Math.sin(angle);
+      //log("xRotated: "+ xRotated +" yRotated"+ yRotated);
+      var drawX = xRotated * width + width/2;
+      var drawY = yRotated * height + height/2;
+      ctx.lineTo(drawX, drawY);
+      //ctx.fillRect(drawX, drawY, 1, 1);
+    }
+
+    ctx.stroke();
+  };
+
+  //
+  // Public
+  //
+  my.start = function(source, canvas) {
+    log("Goniometer.start");
+    log(source);
+    if (raf === undefined) {
+      // split audio context into left and right channel
+      splitChannels(source);
+      ctx = canvas.getContext('2d');
+      //anaL.fftSize = anaR.fftSize = 2048;
+      width = canvas.width = anaL.frequencyBinCount; // both are equal
+      height = canvas.height = 256;
+      if (debug) {
+        my.anaL = anaL;
+        my.anaR = anaR;
+        my.ctx = ctx;
+        my.width = width;
+        my.height = height;
+      }
+      raf = requestAnimationFrame(renderLoop);
+      log("Goniometer started");
+    } else {
+      log("Goniometer already running");
+    }
+  };
+  my.stop = function() {
+    log("Goniometer.stop");
+    if (raf !== undefined) {
+      cancelAnimationFrame(raf);
+      raf = undefined;
+      log("Goniometer stopped");
+    } else {
+      log("Goniometer already stopped");
+    }
+  };
+
+  //
+  // Exit
+  //
+  return my;
+})();
